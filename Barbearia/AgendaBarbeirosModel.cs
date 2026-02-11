f4ab5d6d40261b2932653b0781b5cdc9937dc3df
felipepereirafdpp <felipepereira.fdpp@gmail.com>
2026-02-10 21:17:22 -0300

    Subindo Arquivos Barbearia

---
+++ Barbearia/AgendaBarbeirosModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+namespace AtividadesAPI.Barbearia
+{
+    public class AgendaBarbeirosModel
+    {
+        public AgendaBarbeirosModel(int idBarbeiro, List<string> horariosDisponiveis)
+        {
+            IdBarbeiro = idBarbeiro;
+            HorariosDisponiveis = horariosDisponiveis;
+        }
+
+        public int IdBarbeiro { get;}
+
+        public List<string> HorariosDisponiveis { get; set; } = new();
+
+    }
+}
---
+++ Barbearia/BarbeirosModel.cs
@@ -0,0 +1,20 @@
+﻿namespace AtividadesAPI.Barbearia
+{
+    public class BarbeirosModel
+    {
+        public BarbeirosModel(int id, string nome, string imagemBarbeio, string descricao)
+        {
+            Id = id;
+            Nome = nome;
+            ImagemBarbeio = imagemBarbeio;
+            Descricao = descricao;
+        }
+
+        public int Id { get;}
+        public string Nome { get;}
+        public  string ImagemBarbeio { get;}
+        public string Descricao { get;}
+
+
+    }
+}
